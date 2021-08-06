9d94908ba11ef887a6d10266595fca81c577d778
Marion Livyns <marion.li@hotmail.fr>
2021-08-06 17:41:34 +0100

    06082021 - Indentations,corrections navs,ajout images

---
+++ Header/header.php
@@ -0,0 +1,19 @@
+<header>
+
+            <figure>
+                <img src="images/logo/Reservia@3x.png" alt="LogoReservia" />
+                    <figcaption>
+                        <h1>Trouvez votre hébergement pour des vacances de rêve</h1>
+                        <h2>En centre ville ou en pleine nature</h2>
+                    </figcaption>
+            </figure>
+
+            <nav>
+                <ul>
+                    <li><a href="#">Hébergements</a></li>
+                    <li><a href="#">Activités</a></li>
+                    <li><a href="#">S'inscrire</a></li>
+                </ul>
+            </nav>  
+
+    </header>
